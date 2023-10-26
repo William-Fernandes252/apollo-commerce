e5978e4fbcbc7f4388e8b0224a1ad1e737c76f73
William Fernandes Dias <william.winchester1967@gmail.com>
2023-10-25 23:13:09 -0300

    fix: typo in filter class definition

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,4 +6,4 @@ from products import filters, models, serializers
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
-    filter_set_class = filters.ProductFilter
+    filterset_class = filters.ProductFilter
